1aa4e7a31d6ce42f1632ddfed2f8c2ecb0828416
jfmolano1587 <jose.francisco.molano@gmail.com>
2016-10-18 21:10:33 -0500

    Streaming

--- tweets_to_mongo/historico_seguidores.py
+++ tweets_to_mongo/historico_seguidores.py
@@ -10,7 +10,10 @@ lista_id = collection_tweets.distinct("user.id")
 lista_actual = collection_seguidores.distinct("username")
 conjunto_actual = set(lista_actual)
 for id_user in lista_id:
-	api = urllib2.urlopen("http://api.twittercounter.com/?apikey=e9335031a759f251ee9b4e2e6634e1c5&twitter_id="+str(id_user)).read()
+	try:
+		api = urllib2.urlopen("http://api.twittercounter.com/?apikey=e9335031a759f251ee9b4e2e6634e1c5&twitter_id="+str(id_user)).read()
+	except:
+		print "api error"
 	obj = eval(api)
 	if obj["username"] in conjunto_actual:
 		print "Seguidores ya en BD"
---
+++ tweets_to_mongo/stream.py
@@ -0,0 +1,36 @@
+#Tomado de: http://code.runnable.com/Us9rrMiTWf9bAAW3/how-to-stream-data-from-twitter-with-tweepy-for-python
+import tweepy
+import json
+
+# Authentication details. To  obtain these visit dev.twitter.com
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+# This is the listener, resposible for receiving data
+class StdOutListener(tweepy.StreamListener):
+    def on_data(self, data):
+        # Twitter returns data in JSON format - we need to decode it first
+        decoded = json.loads(data)
+
+        # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
+        print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
+        print ''
+        return True
+
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+    l = StdOutListener()
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    print "Showing all new tweets for #programming:"
+
+    # There are different kinds of streams: public stream, user stream, multi-user streams
+    # In this example follow #programming tag
+    # For more details refer to https://dev.twitter.com/docs/streaming-apis
+    stream = tweepy.Stream(auth, l)
+    stream.filter(track=['paz'])
