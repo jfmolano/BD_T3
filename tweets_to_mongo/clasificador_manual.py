56350f808cbd3246a17623112d1cda8dbeaaa890
jfmolano1587 <jose.francisco.molano@gmail.com>
2016-10-19 12:36:40 -0500

    Clasificador manual y analisis de sentimientos

---
+++ tweets_to_mongo/clasificador_manual.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-s
+from pymongo import MongoClient
+from random import randint
+
+client = MongoClient('localhost', 27017)
+db = client['taller3']
+collection_tweets = db['tweets']
+collection_clasificador = db['clasificador_manual']
+
+cursor = collection_tweets.find()
+cuenta = cursor.count()
+print cursor
+while True:
+	texto = cursor[randint(0,cuenta)]["text"]
+	print texto
+	polaridad = raw_input("Polaridad: ")
+	if polaridad == "s":
+		collection_clasificador.insert({"text":texto,"sentiment":"positive"})
+	if polaridad == "n":
+		collection_clasificador.insert({"text":texto,"sentiment":"negative"})
